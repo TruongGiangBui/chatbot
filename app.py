9400d4949232d5a35b7909329fe5b91f0039e983
TruongGiangBui <55519776+TruongGiangBui@users.noreply.github.com>
2020-11-24 00:52:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,18 +36,19 @@ class ChatBot(Resource):
         global recent
         global builded
 
+        children_left = np.loadtxt("chidlren_left.csv")
+        children_right = np.loadtxt("chidlren_right.csv")
+        feature = np.loadtxt("feture.csv")
+        is_leaves = np.loadtxt("is_leaves.csv")
+        name_feature1 = pd.read_csv("name_feature.csv")
+        name_feature = np.array(name_feature1)
+        name_feature = name_feature.reshape(1, -1)
+        name_feature = name_feature.reshape(131)
+        node_depth = np.loadtxt("node_depth.csv")
+        threshold = np.loadtxt("threshold.csv")
         if int(input)==2 and builded==False:
             builded=True
-            children_left = np.loadtxt("chidlren_left.csv")
-            children_right = np.loadtxt("chidlren_right.csv")
-            feature = np.loadtxt("feture.csv")
-            is_leaves = np.loadtxt("is_leaves.csv")
-            name_feature1 = pd.read_csv("name_feature.csv")
-            name_feature = np.array(name_feature1)
-            name_feature = name_feature.reshape(1, -1)
-            name_feature = name_feature.reshape(131)
-            node_depth = np.loadtxt("node_depth.csv")
-            threshold = np.loadtxt("threshold.csv")
+
             with open("dt.pickle", "rb") as file:
                 model = pickle.load(file)
 
