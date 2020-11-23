9c568805b20b2f9aaef584be7e8717d9039ee41d
TruongGiangBui <55519776+TruongGiangBui@users.noreply.github.com>
2020-11-23 21:56:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,21 +31,7 @@ api=Api(app)
 
 
 class ChatBot(Resource):
-    def __init__(self):
-        super().__init__()
-        global children_left
-        global children_right
-        global feature
-        global is_leaves
-        global name_feature1
-        global name_feature
 
-        global node_depth
-        global threshold
-        global model
-        global X
-        global recent
-        global builded
 
     def get(self,input):
         global children_left
@@ -61,22 +47,6 @@ class ChatBot(Resource):
         global X
         global recent
         global builded
-        builded = True
-        children_left = np.loadtxt("chidlren_left.csv")
-        children_right = np.loadtxt("chidlren_right.csv")
-        feature = np.loadtxt("feture.csv")
-        is_leaves = np.loadtxt("is_leaves.csv")
-        name_feature1 = pd.read_csv("name_feature.csv")
-        name_feature = np.array(name_feature1)
-        name_feature = name_feature.reshape(1, -1)
-        name_feature = name_feature.reshape(131)
-        node_depth = np.loadtxt("node_depth.csv")
-        threshold = np.loadtxt("threshold.csv")
-        with open("dt.pickle", "rb") as file:
-            model = pickle.load(file)
-
-        X = np.zeros(131)
-        recent = 0
 
         if int(input)==2 and builded==False:
             builded=True
@@ -100,7 +70,7 @@ class ChatBot(Resource):
             return {"message":message,"end":False}
         else:
 
-            args = chat.parse_args()
+            
             x = int(input)
             if (x < threshold[recent]):
                 recent = int(children_left[recent])
